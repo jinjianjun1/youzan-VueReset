40ca83a7358b1276055222332a9ab4cad4210eaf
jason <13141374484@163.com>
2018-11-16 09:36:22 +0800

    完善首页

---
+++ youzan/src/pages/index/index.js
@@ -0,0 +1,53 @@
+import 'css/common.css'
+import './index.css'
+import  Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+
+
+import { InfiniteScroll } from 'mint-ui';
+
+Vue.use(InfiniteScroll);
+import foot from 'components/foot.vue'
+
+
+new Vue({
+    el:'#app',
+    data:{
+        lists:null,
+        pageNum:1,
+        pageSize:6,
+        loading:false,//可以继续加载数据,
+        allloaded:false
+    },
+    components:{
+        foot
+    },
+    created(){
+        this.getlists()
+    },
+    methods:{
+        getlists(){
+            if(this.allloaded==true) return
+            this.loading=true
+            axios.get(url.hotLists,{
+                params:{
+                    pageNum:this.pageNum,
+                    pageSize:this.pageSize
+                }
+            }).then(res=>{
+                let currentlist =res.data.lists
+                if(currentlist.length<this.pageSize){
+                    this.allloaded=true
+                }
+                if(this.lists){
+                    this.lists= this.lists.concat(currentlist)
+                }else{
+                    this.lists=currentlist
+                }
+                this.loading=false
+                this.pageNum++
+            })
+        }
+    }
+})
